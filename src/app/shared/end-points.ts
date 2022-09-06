3e22d8112f2cd6f3ecbb14920c4ca8fdfdc349af
aTorrecillas <a.torrecillas@alumnos.upm.es>
2022-09-06 20:56:56 +0200

    Implemented home page #2

--- src/app/shared/end-points.ts
+++ src/app/shared/end-points.ts
@@ -4,6 +4,7 @@ export class EndPoints{
   static SONG = environment.REST_SERVER + '/song';
   static USER = environment.REST_SERVER + '/user';
   static BAND = environment.REST_SERVER + '/band';
+  static BAND_SUMMARY = EndPoints.BAND + '/summary';
   static LOGIN = EndPoints.USER + '/login';
   static REGISTER = EndPoints.USER + '/register';
   static LOGOUT = EndPoints.USER + '/logout';
